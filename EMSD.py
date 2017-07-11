13705955b3e779da00b495a9b11dc7e22f863fc8
Daniel González <DGD042@MacBook-Pro-de-Daniel-Gonzalez-Duque.local>
2017-07-11 14:36:21 -0500

    Change some Functions inside each file

--- EMSD.py
+++ EMSD.py
@@ -1217,6 +1217,79 @@ class EMSD(object):
 
         return DateP_Dict, Value_Dict, Flags_Dict, Flag_Meaning, Stations_Information
 
+    def EDnetCDFFile(self,File,VarDict=None,VarRangeDict=None):
+        '''
+        DESCRIPTION:
+    
+            With this function the information of an IDEAM type file can 
+            be extracted.
+        _______________________________________________________________________
+
+        INPUT:
+            + File: File that would be extracted including the path.
+            + VarDict: List of variables that would be extracted from the 
+                       netCDF file. Defaulted to None.
+            + VarRangeDict: Range of data that would be extracted per 
+                            variable. It is defaulted to None if all the 
+                            Range wants to be extracted.
+                            It must be a list with two values for each variable.
+        _______________________________________________________________________
+        
+        OUTPUT:
+            - Data: Extracted Data Dictionary.    
+        '''
+        # Importing netCDF libary
+        try: 
+            import netCDF4 as nc
+        except:
+            Er = utl.ShowError('EDNCFile','EDSM','netCDF4 not installed, please install the library to continue')
+            raise Er
+
+        # Open File
+        dataset = nc.Dataset(File)
+
+        if VarDict == None:
+            Data = dataset.variables
+        else:
+            Data = dict()
+            for iVar, Var in enumerate(VarDict):
+                try:
+                    P = dataset.variables[Var]
+                except KeyError:
+                    Er = utl.ShowError('EDNCFile','EDSM','Key %s not in the nc file.' %Var)
+                    raise Er
+                if VarRangeDict == None:
+                    Data[Var] = dataset.variables[Var][:]
+                else:
+                    
+                    a = dataset.variables[Var] # Variable
+                    dimensions = a.dimensions # Keys of dimensions
+                    LenD = len(dimensions) # Number of dimensions
+                    totalshape = a.shape # Shape of the matrix
+
+                    Range = dict()
+                    for iVarR,VarR in enumerate(dimensions):
+                        try:
+                            Range[VarR] = VarRangeDict[VarR]
+                        except:
+                            Range[VarR] = [0,dataset.variables[VarR].shape[0]]
+
+                    if LenD == 1:
+                        Data[Var] = dataset.variables[Var][slice(Range[dimensions[0]][0],Range[dimensions[0]][1])]
+                    elif LenD == 2:
+                        Data[Var] = dataset.variables[Var][slice(Range[dimensions[0]][0],Range[dimensions[0]][1])][slice(Range[dimensions[1]][0],Range[dimensions[1]][1])]
+                    elif LenD == 3:
+                        Data[Var] = dataset.variables[Var][slice(Range[dimensions[0]][0],Range[dimensions[0]][1])][slice(Range[dimensions[1]][0],Range[dimensions[1]][1])][slice(Range[dimensions[2]][0],Range[dimensions[2]][1])]
+                    elif LenD == 4:
+                        Data[Var] = dataset.variables[Var][slice(Range[dimensions[0]][0],Range[dimensions[0]][1])][slice(Range[dimensions[1]][0],Range[dimensions[1]][1])][slice(Range[dimensions[2]][0],Range[dimensions[2]][1])][slice(Range[dimensions[3]][0],Range[dimensions[3]][1])]
+                    elif LenD == 5:
+                        Data[Var] = dataset.variables[Var][slice(Range[dimensions[0]][0],Range[dimensions[0]][1])][slice(Range[dimensions[1]][0],Range[dimensions[1]][1])][slice(Range[dimensions[2]][0],Range[dimensions[2]][1])][slice(Range[dimensions[3]][0],Range[dimensions[3]][1])][slice(Range[dimensions[4]][0],Range[dimensions[4]][1])]
+
+
+            dataset.close()
+
+        return Data
+
     # Data Managment
     def CompD(self,Dates,V,dtm=None):
         '''
