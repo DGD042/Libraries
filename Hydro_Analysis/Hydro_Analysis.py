40d873ecae71f05b1bae9d52369b34c18a0761ce
Daniel González <DGD042@MacBook-Pro-de-Daniel-Gonzalez-Duque.local>
2017-09-14 11:31:53 -0500

    Change Functions

--- Hydro_Analysis/Hydro_Analysis.py
+++ Hydro_Analysis/Hydro_Analysis.py
@@ -51,6 +51,7 @@ from Hydro_Analysis.Hydro_Plotter import Hydro_Plotter as HyPl; HyPl=HyPl()
 from Utilities import DatesUtil as DUtil; DUtil=DUtil()
 from Hydro_Analysis.Gen_Functions.Functions import *
 from Hydro_Analysis.Meteo import Cycles as MCy
+from Hydro_Analysis.Climate import Cycles as CCy
 
 class Hydro_Analysis(object):
     '''
@@ -694,28 +695,6 @@ class Hydro_Analysis(object):
         if len(Years) > 2:
             return utl.ShowError('CiclA','Hydro_Analysis','Years index vector larger than 2, review vector')
         
-        # --------------------
-        # Years Managment
-        # --------------------
-        Yi = int(Years[0])
-        Yf = int(Years[1])
-        VarM = np.reshape(VMes,(-1,12))
-        # --------------------
-        # Annual Cycle 
-        # --------------------
-        # Verify NaN data from the cycle
-        MesM = np.empty(12)
-        VarMNT = []
-        for i in range(12):
-            q = sum(~np.isnan(VarM[:,i]))
-            VarMNT.append(sum(~np.isnan(VarM[:,i])))
-            if q <= len(VarM[:,i])*0.70:
-                MesM[i] = np.nan
-            else:
-                MesM[i] = np.nanmean(VarM[:,i]) # Multianual Mean
-        
-        MesD = np.nanstd(VarM,axis=0) # annual strandard deviation.
-        MesE = np.array([k/np.sqrt(VarMNT[ii]) for ii,k in enumerate(MesD)]) # annual Error
         # Graph
         if FlagG:
             HyPl.AnnualCycle(MesM,MesE,VarL,VarLL,Name,NameArch,PathImg,flagAH,color=C)
@@ -723,50 +702,10 @@ class Hydro_Analysis(object):
         # Annual Series
         # --------------------
         if flagA:
-            # Determine operation
-            Operation = utl.Oper_Det(oper)
-            # ----------------
-            # Error managment
-            # ----------------
-            if Operation == -1:
-                return -1
-            # Calculations
-            AnM = np.empty(VarM.shape[0])
-            AnMNT = []
-            for i in range(VarM.shape[0]):
-                q = sum(~np.isnan(VarM[i,:]))
-                if q <= len(VarM[i,:])*0.70:
-                    AnM[i] = np.nan
-                    AnMNT.append(np.nan)
-                else:
-                    AnM[i] = Operation(VarM[i,:])
-                    AnMNT.append(q)
-
-            AnD = np.nanstd(VarM,axis=1) # Annual deviation
-            AnE = np.array([k/np.sqrt(AnMNT[ii]) for ii,k in enumerate(AnD)]) # Annual Error 
-
-            x = [date(i,1,1) for i in range(Yi,Yf+1)]
-            xx = [i for i in range(Yi,Yf+1)]
-
             if FlagG:
                 HyPl.AnnualS(x,AnM,AnE,VarL,VarLL,Name,NameArch,PathImg+'Anual/',color=C)
 
-        # Return values
-        results = dict()
-
-        if flagA:
-            results['MesM'] = MesM 
-            results['MesD'] = MesD 
-            results['MesE'] = MesE 
-            results['AnM'] = AnM
-            results['AnD'] = AnD
-            results['AnE'] = AnE
-            return results
-        else:
-            results['MesM'] = MesM 
-            results['MesD'] = MesD 
-            results['MesE'] = MesE 
-            return results
+        return results
 
     def EstAnom(self,VMes):
         '''
